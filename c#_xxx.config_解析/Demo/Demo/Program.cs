949a3166eef93a3c99c28d6966f66aee4f62e1b6
DaiYan <DaiYan@heng.zhun.com.cn>
2017-04-20 19:33:04 +0800

    add

--- c#_xxx.config_解析/Demo/Demo/Program.cs
+++ c#_xxx.config_解析/Demo/Demo/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Demo
+﻿using System.Configuration;
+
+namespace Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+           ConnectionStringSettings c = System.Configuration.ConfigurationManager.ConnectionStrings["entityFramework"];
         }
     }
 }
